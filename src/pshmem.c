d70de977ae1e8bf8d607160175534fa66843c880
Jeff Hammond <jeff.science@gmail.com>
2014-07-14 00:56:55 -0400

    yet more work pshmem

--- src/pshmem.c
+++ src/pshmem.c
@@ -61,26 +61,16 @@ void shmem_complexf_put(float complex * target, const float complex * source, si
 void shmem_complexd_put(double complex * target, const double complex * source, size_t nelems, int pe){ pshmem_complexd_put(target, source, nelems, pe); }
 
 /* 8.8: Strided Put Routines */
-void shmem_float_iput(float *target, const float *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void pshmem_float_iput(float *target, const float *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void shmem_double_iput(double *target, const double *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void pshmem_double_iput(double *target, const double *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void shmem_longdouble_iput(long double *target, const long double *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void pshmem_longdouble_iput(long double *target, const long double *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void shmem_short_iput(short *target, const short *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void pshmem_short_iput(short *target, const short *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void shmem_int_iput(int *target, const int *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void pshmem_int_iput(int *target, const int *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void shmem_long_iput(long *target, const long *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void pshmem_long_iput(long *target, const long *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void shmem_longlong_iput(long long *target, const long long *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void pshmem_longlong_iput(long long *target, const long long *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void shmem_iput32(void *target, const void *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void pshmem_iput32(void *target, const void *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void shmem_iput64(void *target, const void *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void pshmem_iput64(void *target, const void *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void shmem_iput128(void *target, const void *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
-void pshmem_iput128(void *target, const void *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
+void shmem_float_iput(float *target, const float *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe){ pshmem_float_iput(target, source, tst, sst, len, pe); }
+void shmem_double_iput(double *target, const double *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe) { pshmem_double_iput(target, source, tst, sst, len, pe); }
+void shmem_longdouble_iput(long double *target, const long double *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe){ pshmem_longdouble_iput(target, source, tst, sst, len, pe); }
+void shmem_short_iput(short *target, const short *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe){ pshmem_short_iput(target, source, tst, sst, len, pe); }
+void shmem_int_iput(int *target, const int *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe){ pshmem_int_iput(target, source, tst, sst, len, pe); }
+void shmem_long_iput(long *target, const long *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe){ pshmem_long_iput(target, source, tst, sst, len, pe); }
+void shmem_longlong_iput(long long *target, const long long *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe){ pshmem_longlong_iput(target, source, tst, sst, len, pe); }
+void shmem_iput32(void *target, const void *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe){ pshmem_iput32(target, source, tst, sst, len, pe); }
+void shmem_iput64(void *target, const void *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe){ pshmem_iput64(target, source, tst, sst, len, pe); }
+void shmem_iput128(void *target, const void *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe){ pshmem_iput128(target, source, tst, sst, len, pe); }
 
 /* 8.9: Elemental Data Get Routines */
 float shmem_float_g(float *addr, int pe);
@@ -153,93 +143,58 @@ void shmem_iget128(void *target, const void *source, ptrdiff_t tst, ptrdiff_t ss
 void pshmem_iget128(void *target, const void *source, ptrdiff_t tst, ptrdiff_t sst, size_t len, int pe);
 
 /* 8.12: Atomic Memory fetch-and-operate Routines -- Swap */
-float shmem_float_swap(float *target, float value, int pe);
-float pshmem_float_swap(float *target, float value, int pe);
-double shmem_double_swap(double *target, double value, int pe);
-double pshmem_double_swap(double *target, double value, int pe);
-int shmem_int_swap(int *target, int value, int pe);
-int pshmem_int_swap(int *target, int value, int pe);
-long shmem_long_swap(long *target, long value, int pe);
-long pshmem_long_swap(long *target, long value, int pe);
-long long shmem_longlong_swap(long long *target, long long value, int pe);
-long long pshmem_longlong_swap(long long *target, long long value, int pe);
-long shmem_swap(long *target, long value, int pe);
-long pshmem_swap(long *target, long value, int pe);
+float shmem_float_swap(float *target, float value, int pe){ return pshmem_float_swap(target, value, pe); }
+double shmem_double_swap(double *target, double value, int pe){ return pshmem_double_swap(target, value, pe); }
+int shmem_int_swap(int *target, int value, int pe){ return pshmem_int_swap(target, value, pe); }
+long shmem_long_swap(long *target, long value, int pe){ return pshmem_long_swap(target, value, pe); }
+long long shmem_longlong_swap(long long *target, long long value, int pe){ return pshmem_longlong_swap(target, value, pe); }
+long shmem_swap(long *target, long value, int pe){ return pshmem_swap(target, value, pe); }
 
 /* 8.12: Atomic Memory fetch-and-operate Routines -- Cswap */
-int shmem_int_cswap(int *target, int cond, int value, int pe);
-int pshmem_int_cswap(int *target, int cond, int value, int pe);
-long shmem_long_cswap(long *target, long cond, long value, int pe);
-long pshmem_long_cswap(long *target, long cond, long value, int pe);
-long long shmem_longlong_cswap(long long * target, long long cond, long long value, int pe);
-long long pshmem_longlong_cswap(long long * target, long long cond, long long value, int pe);
+int shmem_int_cswap(int *target, int cond, int value, int pe){ pshmem_int_cswap(target, cond, value, pe); }
+long shmem_long_cswap(long *target, long cond, long value, int pe){ pshmem_long_cswap(target, cond, value, pe); }
+long long shmem_longlong_cswap(long long * target, long long cond, long long value, int pe){ pshmem_longlong_cswap(target, cond, value, pe); }
 
 /* 8.12: Atomic Memory fetch-and-operate Routines -- Fetch and Add */
-int shmem_int_fadd(int *target, int value, int pe);
-int pshmem_int_fadd(int *target, int value, int pe);
-long shmem_long_fadd(long *target, long value, int pe);
-long pshmem_long_fadd(long *target, long value, int pe);
-long long shmem_longlong_fadd(long long *target, long long value, int pe);
-long long pshmem_longlong_fadd(long long *target, long long value, int pe);
+int shmem_int_fadd(int *target, int value, int pe){ return pshmem_int_fadd(target, value, pe); }
+long shmem_long_fadd(long *target, long value, int pe){ return pshmem_long_fadd(target, value, pe); }
+long long shmem_longlong_fadd(long long *target, long long value, int pe){ return pshmem_longlong_fadd(target, value, pe); }
 
 /* 8.12: Atomic Memory fetch-and-operate Routines -- Fetch and Increment */
-int shmem_int_finc(int *target, int pe);
-int pshmem_int_finc(int *target, int pe);
-long shmem_long_finc(long *target, int pe);
-long pshmem_long_finc(long *target, int pe);
-long long shmem_longlong_finc(long long *target, int pe);
-long long pshmem_longlong_finc(long long *target, int pe);
+int shmem_int_finc(int *target, int pe){ return pshmem_int_finc(target, pe); }
+long shmem_long_finc(long *target, int pe){ return pshmem_long_finc(target, pe); }
+long long shmem_longlong_finc(long long *target, int pe){ return pshmem_longlong_finc(target, pe); }
 
 /* 8.13: Atomic Memory Operation Routines -- Add */
-void shmem_int_add(int *target, int value, int pe);
-void pshmem_int_add(int *target, int value, int pe);
-void shmem_long_add(long *target, long value, int pe);
-void pshmem_long_add(long *target, long value, int pe);
-void shmem_longlong_add(long long *target, long long value, int pe);
-void pshmem_longlong_add(long long *target, long long value, int pe);
+void shmem_int_add(int *target, int value, int pe){ pshmem_int_add(target, value, pe); }
+void shmem_long_add(long *target, long value, int pe){ pshmem_long_add(target, value, pe); }
+void shmem_longlong_add(long long *target, long long value, int pe){ pshmem_longlong_add(target, value, pe); }
 
 /* 8.13: Atomic Memory Operation Routines -- Increment */
-void shmem_int_inc(int *target, int pe);
-void pshmem_int_inc(int *target, int pe);
-void shmem_long_inc(long *target, int pe);
-void pshmem_long_inc(long *target, int pe);
-void shmem_longlong_inc(long long *target, int pe);
-void pshmem_longlong_inc(long long *target, int pe);
+void shmem_int_inc(int *target, int pe){ pshmem_int_inc(target, pe); }
+void shmem_long_inc(long *target, int pe){ pshmem_long_inc(target, pe); }
+void shmem_longlong_inc(long long *target, int pe){ pshmem_longlong_inc(target, pe); }
 
 /* 8.14: Point-to-Point Synchronization Routines -- Wait*/
-void shmem_short_wait(short *var, short value);
-void pshmem_short_wait(short *var, short value);
-void shmem_int_wait(int *var, int value);
-void pshmem_int_wait(int *var, int value);
-void shmem_long_wait(long *var, long value);
-void pshmem_long_wait(long *var, long value);
-void shmem_longlong_wait(long long *var, long long value);
-void pshmem_longlong_wait(long long *var, long long value);
-void shmem_wait(long *ivar, long cmp_value);
-void pshmem_wait(long *ivar, long cmp_value);
+void shmem_short_wait(short *var, short value){ pshmem_short_wait(var, value); }
+void shmem_int_wait(int *var, int value){ pshmem_int_wait(var, value); }
+void shmem_long_wait(long *var, long value){ pshmem_long_wait(var, value); }
+void shmem_longlong_wait(long long *var, long long value){ pshmem_longlong_wait(var, value); }
+void shmem_wait(long *ivar, long cmp_value){ pshmem_wait(ivar, cmp_value); }
 
 /* 8.14: Point-to-Point Synchronization Routines -- Wait Until*/
-void shmem_short_wait_until(short *var, int cond, short value);
-void pshmem_short_wait_until(short *var, int cond, short value);
-void shmem_int_wait_until(int *var, int cond, int value);
-void pshmem_int_wait_until(int *var, int cond, int value);
-void shmem_long_wait_until(long *var, int cond, long value);
-void pshmem_long_wait_until(long *var, int cond, long value);
-void shmem_longlong_wait_until(long long *var, int cond, long long value);
-void pshmem_longlong_wait_until(long long *var, int cond, long long value);
-void shmem_wait_until(long *ivar, int cmp, long value);
-void pshmem_wait_until(long *ivar, int cmp, long value);
+void shmem_short_wait_until(short *var, int cond, short value){ pshmem_short_wait_until(var, cond, value); }
+void shmem_int_wait_until(int *var, int cond, int value){ pshmem_int_wait_until(var, cond, value); }
+void shmem_long_wait_until(long *var, int cond, long value){ pshmem_long_wait_until(var, cond, value); }
+void shmem_longlong_wait_until(long long *var, int cond, long long value){ pshmem_longlong_wait_until(var, cond, value); }
+void shmem_wait_until(long *ivar, int cmp, long value){ pshmem_wait_until(ivar, cmp, value); }
 
 /* 8.15: Barrier Synchronization Routines */
-void shmem_barrier(int PE_start, int logPE_stride, int PE_size, long *pSync);
-void pshmem_barrier(int PE_start, int logPE_stride, int PE_size, long *pSync);
-void shmem_barrier_all(void);
-void pshmem_barrier_all(void);
+void shmem_barrier(int PE_start, int logPE_stride, int PE_size, long *pSync){ pshmem_barrier(PE_start, logPE_stride, PE_size, pSync); }
+void shmem_barrier_all(void){ pshmem_barrier_all(); }
 
-void shmem_quiet(void);
-void pshmem_quiet(void);
-void shmem_fence(void);
-void pshmem_fence(void);
+void shmem_quiet(void){ pshmem_quiet(); }
+void shmem_fence(void){ pshmem_fence(); }
 
 /* 8.16: Reduction Routines */
 void shmem_short_and_to_all(short *target, short *source, int nreduce,
@@ -540,49 +495,36 @@ void pshmem_fcollect64(void *target, const void *source, size_t nlong,
 /* 8.18: Broadcast Routines */
 void shmem_broadcast32(void *target, const void *source, size_t nlong, 
                        int PE_root, int PE_start, int logPE_stride,
-                       int PE_size, long *pSync);
-void pshmem_broadcast32(void *target, const void *source, size_t nlong, 
-                        int PE_root, int PE_start, int logPE_stride,
-                        int PE_size, long *pSync);
+                       int PE_size, long *pSync){
+    pshmem_broadcast32(target, source, nlong, PE_root,PE_start, logPE_stride, PE_size, pSync);
+}
 void shmem_broadcast64(void *target, const void *source, size_t nlong,
                        int PE_root, int PE_start, int logPE_stride,
-                       int PE_size, long *pSync);
-void pshmem_broadcast64(void *target, const void *source, size_t nlong,
-                        int PE_root, int PE_start, int logPE_stride,
-                        int PE_size, long *pSync);
+                       int PE_size, long *pSync){ 
+    pshmem_broadcast64(target, source, nlong, PE_root,PE_start, logPE_stride, PE_size, pSync);
+}
 
 /* 8.19: Lock Routines */
-void shmem_set_lock(long *lock);
-void pshmem_set_lock(long *lock);
-void shmem_clear_lock(long *lock);
-void pshmem_clear_lock(long *lock);
-int shmem_test_lock(long *lock);
-int pshmem_test_lock(long *lock);
+void shmem_set_lock(long *lock){ pshmem_set_lock(lock); }
+void shmem_clear_lock(long *lock){ pshmem_clear_lock(lock); }
+int shmem_test_lock(long *lock){ return pshmem_test_lock(lock); }
 
 /* A.1: Cache Management Routines (deprecated) */
-void shmem_set_cache_inv(void) __attribute__ ((deprecated));
-void pshmem_set_cache_inv(void) __attribute__ ((deprecated));
-void shmem_set_cache_line_inv(void *target) __attribute__ ((deprecated));
-void pshmem_set_cache_line_inv(void *target) __attribute__ ((deprecated));
-void shmem_clear_cache_inv(void) __attribute__ ((deprecated));
-void pshmem_clear_cache_inv(void) __attribute__ ((deprecated));
-void shmem_clear_cache_line_inv(void *target) __attribute__ ((deprecated));
-void pshmem_clear_cache_line_inv(void *target) __attribute__ ((deprecated));
-void shmem_udcflush(void) __attribute__ ((deprecated));
-void pshmem_udcflush(void) __attribute__ ((deprecated));
-void shmem_udcflush_line(void *target) __attribute__ ((deprecated));
-void pshmem_udcflush_line(void *target) __attribute__ ((deprecated));
+#if DEPRECATED
+void shmem_set_cache_inv(void) { pshmem_set_cache_inv(); }
+void shmem_set_cache_line_inv(void *target){ pshmem_set_cache_line_inv(target); }
+void shmem_clear_cache_inv(void){ pshmem_clear_cache_inv(); }
+void shmem_clear_cache_line_inv(void *target){ pshmem_clear_cache_line_inv(target); }
+void shmem_udcflush(void){ pshmem_udcflush(); }
+void shmem_udcflush_line(void *target){ pshmem_udcflush_line(target); }
+#endif
 
  /* Portals extensions */
-double shmem_wtime(void);
-double pshmem_wtime(void);
-char* shmem_nodename(void);
-char* pshmem_nodename(void);
+double shmem_wtime(void){ return pshmem_wtime(); }
+char* shmem_nodename(void){ return pshmem_nodename(); }
 
 #if 0 // NOT IMPLEMENTED IN MPI-3 VERSION
 /* Signalling puts */
-typedef char * shmem_ct_t;
-
 void shmem_putmem_ct(pshmem_ct_t ct, void *target, const void *source, size_t len, int pe);
 void pshmem_putmem_ct(pshmem_ct_t ct, void *target, const void *source, size_t len, int pe);
 void shmem_ct_create(pshmem_ct_t *ct);
