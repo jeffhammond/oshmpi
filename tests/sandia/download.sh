c3e3b86bd64026dad7cfa873f83d87a6d5fe2f90
Jeff Hammond <jeff.science@gmail.com>
2016-02-10 21:13:04 -0800

    clone 1.3 branch [ci skip]

--- tests/sandia/download.sh
+++ tests/sandia/download.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git clone https://github.com/Sandia-OpenSHMEM/SOS sandia-openshmem
+BRANCH=v1.3.0-dev
+git clone -b $(BRANCH) --depth 10 https://github.com/Sandia-OpenSHMEM/SOS sandia-openshmem
 mv sandia-openshmem/test/unit/*.c .
 mv sandia-openshmem/test/performance/*.c .
 rm -rf sandia-openshmem
